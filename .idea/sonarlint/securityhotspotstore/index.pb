af40bf8c503e26313f2de8943fede43cc6b176d9
Maxi Sandoval <maxisandoval98@gmail.com>
2023-12-28 20:06:34 -0300

    commit inicial

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+w
+Gsrc/main/java/dev/maxisandoval/soapppdemoproject/SOAPPPDemoProject.java,4\2\42d0cfa85a68c4f174b9cd7f4b1bab9e481d80d1
---
+++ src/main/java/dev/maxisandoval/soapppdemoproject/SOAPPPDemoProject.java
@@ -0,0 +1,12 @@
+package dev.maxisandoval.soapppdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SOAPPPDemoProject {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SOAPPPDemoProject.class, args);
+	}
+}
